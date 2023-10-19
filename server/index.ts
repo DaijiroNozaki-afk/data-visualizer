47dd3845e4e86500437bd8677b19bc50523c38af
dnozaki <dnozaki88888888@gmail.com>
2023-10-19 16:17:40 +0900

    first commit

---
+++ server/index.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+const app = express();
+const port = 5001;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listhening on port ${port}!`));
